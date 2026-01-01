474b45e7b8417e638a8cda819bbb8ce7c275178a
Gabriel092005 <gabrielcavalamanuel@gmail.com>
2026-01-01 19:29:55 +0000

    notif

--- src/pages/app/dashboard-admin/sidebar/Notification/notif-dropdown-costumer.tsx
+++ src/pages/app/dashboard-admin/sidebar/Notification/notif-dropdown-costumer.tsx
@@ -3,7 +3,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import { ScrollArea } from "@/components/ui/scroll-area";
